78d843fdaa2021162ae5eac3d8d926ff97ebfecf
Stiigel <rikasoykkari@gmail.com>
2014-04-04 19:33:05 +0300

    ommanepemehum

--- utube.py
+++ utube.py
@@ -19,13 +19,20 @@ class Utube:
     self.kerrat = []
     self.sittenit = []
     
+    self.ehdotukset = []
+    self.ed = {"otsake" : "", "linkki" : ""}
+    
   def laita_video(self):
     self.video = not self.video
-     
-  def ota_video(self):
-    return self.video  
   
   def kasittele_haku(self, haku):    
+    self.linkit = []
+    self.ajat = []
+    self.tekijat = []
+    self.otsakkeet = []
+    self.kerrat = []
+    self.sittenit = []
+    
     parametrit = { "search_query" : haku, "filters" : "video", "lclk" : "video", "hl" : "fi" }
     responssi = requests.get("https://www.youtube.com/results", params=parametrit, verify=False)
     teksti = responssi.text.split("\n")
@@ -67,14 +74,62 @@ class Utube:
             self.kerrat.append(re.search('<li>(.*?) näyttökertaa</li>',teksti[i]).group(1))
             tekijaAlku = -1
             kohta = -1 
+            
+  def kasittele_ehdotukset(self):
+    if self.ed["linkki"] == "":
+      print("¡Hae ensin!")
+      return
+    
+    self.ehdotukset = []
+    
+    responssi = requests.get(self.ed["linkki"], verify=False)
+    teksti = responssi.text.split("\n")
+    
+    kohta = -1
+    tekijaKohta = -1
+    mones = -1
+    for i in range(len(teksti)):
+      
+      if '"related-video  spf-link yt-uix-sessionlink"' in teksti[i]:
+        mones += 1
+        kohta = i
+        self.ehdotukset.append({})      
+      
+      if kohta != -1:
+        if 'href="/watch?v=' in teksti[i]:
+          linkki = re.search('href="/watch\?v=(.*?)"', teksti[i]).group(1)
+          self.ehdotukset[mones]["linkki"] = linkki
+          
+        if '<span class="video-time">' in teksti[i]:
+          aika = re.search('<span class="video-time">(.*?)</span>', teksti[i]).group(1)
+          self.ehdotukset[mones]["aika"] = aika
+          
+        if 'class="title" title="' in teksti[i]:
+          otsake = re.search('class="title" title="(.*?)"', teksti[i]).group(1)
+          self.ehdotukset[mones]["otsake"] = otsake
         
+        if '<span class="stat view-count">' in teksti[i]:
+          kerrat = re.search('view-count">(.*?) näyttökertaa</span>', teksti[i]).group(1)
+          self.ehdotukset[mones]["kerrat"] = kerrat    
+          kohta = -1
+          
+        if "tekijä" in teksti[i]:
+          tekijaKohta = i
+          
+        if tekijaKohta != -1:
+          if "</span>" in teksti[i]:
+            tekija = re.search('">(.*?)</span>', teksti[i]).group(1)
+            self.ehdotukset[mones]["tekija"] = tekija
+            tekijaKohta = -1
+         
+   
+              
   def ota_otsake(self, linkki):
     responssi = requests.get(linkki, verify=False)
     teksti = responssi.text
     
     alku = re.search("<title>",teksti).end()
     loppu = re.search("</title>",teksti).start()
-
     return teksti[alku : loppu]
     
   def kuuntele_kpl(self, mones, linkki=""):
@@ -93,6 +148,9 @@ class Utube:
     
     subprocess.call(kaskyt)
     
+    self.ed["otsake"] = otsake
+    self.ed["linkki"] = linkki
+    
     
   def lataa_kpl(self, mones, linkki=""):
     if linkki == "":
@@ -104,6 +162,21 @@ class Utube:
     
     subprocess.call(kaskyt)
     
+    self.ed["otsake"] = otsake
+    self.ed["linkki"] = linkki
+    
+  def nayta_ehdotukset(self,monta):
+    if len(self.ehdotukset) == 0:
+      return
+    for i in range(monta):
+      if len(self.ehdotukset) >= i:
+        aika = self.ehdotukset[i]["aika"]
+        tekija = self.ehdotukset[i]["tekija"]
+        otsake = self.ehdotukset[i]["otsake"]
+        linkki = self.ehdotukset[i]["linkki"]
+        kerrat = self.ehdotukset[i]["kerrat"]
+        print(str(i + 1) + ". tulos: " + otsake + " | " + aika + " | " + tekija)
+        
   def nayta_tulokset(self, monta):
     for i in range(monta):
       if len(self.tekijat) >= i:
